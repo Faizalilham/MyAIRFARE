bebde6a69e521caf53efa773d2bdb7e169607424
Fani Aghistiya Lestari <fani.aghistiya.lestr@gmail.com>
2022-12-29 21:28:18 +0700

    Unit Testing Login Activity

---
+++ app/src/test/java/binar/finalproject/MyAirFare/ui/activities/LoginActivityTest.kt
@@ -0,0 +1,54 @@
+package binar.finalproject.MyAirFare.ui.activities
+
+import binar.finalproject.MyAirFare.api.AuthEndPoint
+import binar.finalproject.MyAirFare.model.login.UserLoginRequest
+import binar.finalproject.MyAirFare.model.login.UserLoginResponse
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+import retrofit2.Call
+
+internal class LoginActivityTest{
+    private lateinit var service: AuthEndPoint
+
+    @Before
+    fun setUp() {
+        service = mockk()
+    }
+
+    @Test
+    fun getUser(): Unit = runBlocking {
+        val response = mockk<Call<UserLoginResponse>>()
+        every {
+            service.doUserLogin(
+                UserLoginRequest(
+                    email = "@mail.com",
+                    password = "pass"
+                )
+            )
+        } returns response
+
+        val result = service.doUserLogin(
+            UserLoginRequest(
+                email = "@mail.com",
+                password = "pass"
+            )
+        )
+
+        verify {
+            service.doUserLogin(
+                UserLoginRequest(
+                    email = "@mail.com",
+                    password = "pass"
+                )
+            )
+        }
+        Assert.assertEquals(result, response)
+    }
+}
+
